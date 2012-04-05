664a635cc446b9889e42acb778b2d92664319a30
Alexander Surma <surma@surmair.de>
2012-04-05 11:45:47 +0200

    Remove hg and bzr based packages

--- app.go
+++ app.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"code.google.com/p/gorilla/mux"
-	testpkg "launchpad.net/heroku-buildpack-go-testpkg"
+	// "code.google.com/p/gorilla/mux"
+	// testpkg "launchpad.net/heroku-buildpack-go-testpkg"
 	"net/http"
 	"os"
 	"heroku-buildpack-go-app/consts"
@@ -10,14 +10,20 @@ import (
 )
 
 func main() {
-	r := mux.NewRouter()
-	r.HandleFunc("/", handler)
-	e := http.ListenAndServe(":"+os.Getenv("PORT"), r)
+	// r := mux.NewRouter()
+	// r.HandleFunc("/", handler)
+	// e := http.ListenAndServe(":"+os.Getenv("PORT"), r)
+	// if e != nil {
+		// panic(e)
+	// }
+	http.HandleFunc("/", handler)
+	e := http.ListenAndServe(":"+os.Getenv("PORT"), nil)
 	if e != nil {
 		panic(e)
 	}
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, consts.MESSAGE+"\n"+testpkg.GiveMeMyString())
+	// fmt.Fprintf(w, consts.MESSAGE+"\n"+testpkg.GiveMeMyString())
+	fmt.Fprintf(w, consts.MESSAGE)
 }
